430e38833e573d60894ec8965e50a37535c5bdca
Dávid Kecskés <32202941+kecskesd@users.noreply.github.com>
2020-11-11 13:52:58 +0100

    Katka upravy

--- renderer.js
+++ renderer.js
@@ -110,14 +110,6 @@ let lines3 = new Chart(document.getElementById("chart3"), {
 });
 
 //win.maximize();
-document.getElementsByClassName('component')[0].style.opacity = '0.5';
-document.getElementsByClassName('component')[1].style.opacity = '0.5';
-document.getElementsByClassName('component')[2].style.opacity = '0.5';
-document.getElementsByClassName('component')[3].style.opacity = '0.5';
-document.getElementsByClassName('component')[0].style.background = '#f5f5f5';
-document.getElementsByClassName('component')[1].style.background = '#f5f5f5';
-document.getElementsByClassName('component')[2].style.background = '#f5f5f5';
-document.getElementsByClassName('component')[3].style.background = '#f5f5f5';
 
 document.getElementById('max-button').addEventListener("click", event => {
     if(win.isMaximized())
@@ -134,6 +126,37 @@ document.getElementById('close-button').addEventListener("click", event => {
     win.close();
 });
 
+document.getElementById('komponent').addEventListener("click", event => {
+    document.getElementById('w0').style.display = "none";
+    document.getElementById('w1').style.display = "block";
+    document.getElementById('w2').style.display = "none";
+});
+
+document.getElementById('draha').addEventListener("click", event => {
+    document.getElementById('name').innerHTML = "Dráha";
+    document.getElementById('w0').style.display = "none";
+    document.getElementById('w1').style.display = "none";
+    document.getElementById('w2').style.display = "block";
+    document.getElementById('ww1').style.display = "block";
+    document.getElementById('ww2').style.display = "none";
+});
+
+
+document.getElementById('open-button0').addEventListener("click", event => {
+    dialog.showOpenDialog({
+        properties: ['openFile'],
+        filters: [
+            { name: 'Log File (.log)', extensions: ['log'] },
+        ]
+    }).then(result => {
+        if (!result.canceled) {
+            document.getElementById('file-name0').innerHTML = result.filePaths.toString().split(/(.*)\\/)[2].split(/\.log$/)[0];
+        }
+    }).catch(err => {
+        console.log(err)
+    })
+});
+
 document.getElementById('open-button1').addEventListener("click", event => {
     dialog.showOpenDialog({
         properties: ['openFile'],
@@ -143,26 +166,6 @@ document.getElementById('open-button1').addEventListener("click", event => {
     }).then(result => {
         if (!result.canceled) {
             document.getElementById('file-name1').innerHTML = result.filePaths.toString().split(/(.*)\\/)[2].split(/\.log$/)[0];
-            file1 = true;
-            document.getElementsByClassName('component')[0].style.transition = "0.5s ease-in-out";
-            document.getElementsByClassName('component')[1].style.transition = "0.5s ease-in-out";
-            document.getElementsByClassName('component')[2].style.transition = "0.5s ease-in-out";
-            document.getElementsByClassName('component')[0].style.opacity = '1';
-            document.getElementsByClassName('component')[1].style.opacity = '1';
-            document.getElementsByClassName('component')[2].style.opacity = '1';
-            document.getElementsByClassName('component')[0].style.background = null;
-            document.getElementsByClassName('component')[1].style.background = null;
-            document.getElementsByClassName('component')[2].style.background = null;
-            document.getElementsByClassName('component')[0].style.cursor = 'pointer';
-            document.getElementsByClassName('component')[1].style.cursor = 'pointer';
-            document.getElementsByClassName('component')[2].style.cursor = 'pointer';
-            document.getElementById('line').style.borderLeft = "1px solid #dddddd";
-            if (file2) {
-                document.getElementsByClassName('component')[3].style.transition = "0.5s ease-in-out";
-                document.getElementsByClassName('component')[3].style.opacity = '1';
-                document.getElementsByClassName('component')[3].style.background = null;
-                document.getElementsByClassName('component')[3].style.cursor = 'pointer';
-            }
         }
     }).catch(err => {
         console.log(err)
@@ -178,63 +181,43 @@ document.getElementById('open-button2').addEventListener("click", event => {
     }).then(result => {
         if (!result.canceled) {
             document.getElementById('file-name2').innerHTML = result.filePaths.toString().split(/(.*)\\/)[2].split(/\.log$/)[0];
-            file2 = true;
-            if (file1) {
-                document.getElementsByClassName('component')[3].style.transition = "0.5s ease-in-out";
-                document.getElementsByClassName('component')[3].style.opacity = '1';
-                document.getElementsByClassName('component')[3].style.background = null;
-                document.getElementsByClassName('component')[3].style.cursor = 'pointer';
-                document.getElementById('line').style.borderLeft = "1px solid #dddddd";
-            }
         }
     }).catch(err => {
         console.log(err)
     })
 });
 
+document.getElementById('back-button').addEventListener("click", event => {
+    document.getElementById('w0').style.display = "block";
+    document.getElementById('w1').style.display = "none";
+    document.getElementById('w2').style.display = "none";
+});
+
 document.getElementById('c1').addEventListener("click", event => {
-    if (file1) {
-        document.getElementById('name').innerHTML = "Koleso";
-        document.getElementById('w1').style.display = "none";
-        document.getElementById('w2').style.display = "block";
-        document.getElementById('ww1').style.display = "none";
-        document.getElementById('ww2').style.display = "block";
-    }
+    document.getElementById('name').innerHTML = "Koleso";
+    document.getElementById('w0').style.display = "none";
+    document.getElementById('w1').style.display = "none";
+    document.getElementById('w2').style.display = "block";
+    document.getElementById('ww1').style.display = "none";
+    document.getElementById('ww2').style.display = "block";
 });
 
 document.getElementById('c2').addEventListener("click", event => {
-    if (file1) {
-        document.getElementById('name').innerHTML = "Volant";
-        document.getElementById('w1').style.display = "none";
-        document.getElementById('w2').style.display = "block";
-        document.getElementById('ww1').style.display = "none";
-        document.getElementById('ww2').style.display = "block";
-    }
+    document.getElementById('name').innerHTML = "Volant";
+    document.getElementById('w0').style.display = "none";
+    document.getElementById('w1').style.display = "none";
+    document.getElementById('w2').style.display = "block";
+    document.getElementById('ww1').style.display = "none";
+    document.getElementById('ww2').style.display = "block";
 });
 
 document.getElementById('c3').addEventListener("click", event => {
-    if (file1) {
-        document.getElementById('name').innerHTML = "Brzdy";
-        document.getElementById('w1').style.display = "none";
-        document.getElementById('w2').style.display = "block";
-        document.getElementById('ww1').style.display = "none";
-        document.getElementById('ww2').style.display = "block";
-    }
-});
-
-document.getElementById('c4').addEventListener("click", event => {
-    if (file1 && file2) {
-        document.getElementById('name').innerHTML = "Dráha";
-        document.getElementById('w1').style.display = "none";
-        document.getElementById('w2').style.display = "block";
-        document.getElementById('ww1').style.display = "block";
-        document.getElementById('ww2').style.display = "none";
-    }
-});
-
-document.getElementById('back-button').addEventListener("click", event => {
-    document.getElementById('w1').style.display = "block";
-    document.getElementById('w2').style.display = "none";
+    document.getElementById('name').innerHTML = "Brzdy";
+    document.getElementById('w0').style.display = "none";
+    document.getElementById('w1').style.display = "none";
+    document.getElementById('w2').style.display = "block";
+    document.getElementById('ww1').style.display = "none";
+    document.getElementById('ww2').style.display = "block";
 });
 
 document.getElementById('print-button').addEventListener("click", event => {
