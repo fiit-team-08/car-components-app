8ea353952f1d4f984596ac4c000b6b5607e8a343
Dávid Kecskés <32202941+kecskesd@users.noreply.github.com>
2021-03-29 18:01:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import eel
+from mpc.mpc import *
 from analysis.log_file_analyzer import *
+
 
 eel.init('electron')
 eel.browsers.set_path('electron', 'node_modules/electron/dist/electron')
 
 laps = None
-
+mpc_data = None
 
 @eel.expose
 def getpath(path):
@@ -16,6 +18,34 @@ def getpath(path):
 @eel.expose
 def get_track_data(path):
     json = get_track_graph_data(path)
+    return json
+
+
+@eel.expose
+def get_mpc_ref_xy(path):
+    json = get_mpc_reference_xy(get_reference_data(path))
+    return json
+
+
+@eel.expose
+def get_mpc_ref_crs(path):
+    json = get_mpc_reference_crs(get_reference_data(path))
+    return json
+
+
+@eel.expose
+def get_mpc_xy(path, length, width, backtowheel, wb, wheel, target_speed, max_speed, max_accel, max_steer, max_dsteer):
+    global mpc_data
+    mpc_data = mpc(path, length, width, backtowheel, wb, wheel, target_speed, max_speed, max_accel, max_steer, max_dsteer)
+    temp = mpc_data.copy()
+    json = get_mpc_data_xy(temp)
+    return json
+
+
+@eel.expose
+def get_mpc_crs(referenceFileName):
+    temp = mpc_data.copy()
+    json = get_mpc_data_crs(temp)
     return json
 
 
@@ -34,6 +64,12 @@ def export_data(path, file_name):
         return
     save_laps_to_files(path, file_name, laps)
 
+
+@eel.expose
+def export_mpc(path, file_name):
+    if mpc_data is None:
+        return
+    mpc_data.to_csv(path+'\out.csv', index=False)
 
 
 @eel.expose
